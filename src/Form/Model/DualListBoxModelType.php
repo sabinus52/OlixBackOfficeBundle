05112307430c5f23a2aa820bbab5b60e66dfa552
Sabinus52 <sabinus52@gmail.com>
2022-05-29 16:49:26 +0200

    Formalize DualListBox

--- src/Form/Model/DualListBoxModelType.php
+++ src/Form/Model/DualListBoxModelType.php
@@ -11,23 +11,43 @@ declare(strict_types=1);
 
 namespace Olix\BackOfficeBundle\Form\Model;
 
-use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Widget de formulaire de selection multiple en double liste.
  *
+ * @example     Configuration with options of this type
+ * @example     @param string       ojs_filter_text_clear       : The text for the "Show All" button
+ * @example     @param string       ojs_filter_place_holder     : The placeholder for the input element for filtering elements
+ * @example     @param string       ojs_move_selected_label     : The label for the "Move Selected" button
+ * @example     @param string       ojs_move_all_label          : The label for the "Move All" button
+ * @example     @param string       ojs_remove_selected_label   : The label for the "Remove Selected" button
+ * @example     @param string       ojs_remove_all_label        : The label for the "Remove All" button
+ * @example     @param string|bool  ojs_selected_list_label     : Can be a string specifying the name of the selected list
+ * @example     @param string|bool  ojs_non_selected_list_label : Can be a string specifying the name of the non selected list
+ * @example     @param int          ojs_selector_minimal_height : Represents the minimal height of the generated dual listbox
+ * @example     @param bool         ojs_show_filter_inputs      : Whether to show filter input
+ * @example     @param string       ojs_non_selected_filter     : Initializes the dual listbox with a filter for the non selected elements
+ * @example     @param string       ojs_selected_filter         : Initializes the dual listbox with a filter for the selected elements
+ * @example     @param string|bool  ojs_info_text               : Set this to false to hide this information
+ * @example     @param string       ojs_info_text_filtered      : Determines which element format to use when some element is filtered
+ * @example     @param string       ojs_info_text_empty         : Determines the string to use when there are no options in the list
+ * @example     @param string       ojs_filter_on_values        : Set this to true to filter the options according to their values
+ *
  * @author      Sabinus52 <sabinus52@gmail.com>
  *
  * @see         https://github.com/istvan-ujjmeszaros/bootstrap-duallistbox
  */
-abstract class DualListBoxModelType extends AbstractType
+abstract class DualListBoxModelType extends AbstractModelType
 {
     /**
      * {@inheritDoc}
      */
     public function configureOptions(OptionsResolver $resolver): void
     {
+        // Options du widget du formulaire
         $resolver->setDefaults([
             'multiple' => true,
             'expanded' => false,
@@ -35,6 +55,52 @@ abstract class DualListBoxModelType extends AbstractType
 
         $resolver->setAllowedValues('multiple', [true]);
         $resolver->setAllowedValues('expanded', [false]);
+
+        // Options JavaScript supplémentaires du widget
+        $resolver->setDefaults([
+            'ojs_filter_text_clear' => 'voir tous',
+            'ojs_filter_place_holder' => 'Filtrer',
+            'ojs_move_selected_label' => 'Déplacer la sélection',
+            'ojs_move_all_label' => 'Déplacer tous',
+            'ojs_remove_selected_label' => 'Supprimer la sélection',
+            'ojs_remove_all_label' => 'Supprimer tous',
+            'ojs_selected_list_label' => false,
+            'ojs_non_selected_list_label' => false,
+            'ojs_selector_minimal_height' => 100,
+            'ojs_show_filter_inputs' => true,
+            'ojs_non_selected_filter' => '',
+            'ojs_selected_filter' => '',
+            'ojs_info_text' => 'Voir tous {0}',
+            'ojs_info_text_filtered' => '<span class="badge badge-warning">Filtré</span> {0} sur {1}',
+            'ojs_info_text_empty' => 'Liste vide',
+            'ojs_filter_on_values' => false,
+        ]);
+
+        $resolver->setAllowedTypes('ojs_filter_text_clear', ['string']);
+        $resolver->setAllowedTypes('ojs_filter_place_holder', ['string']);
+        $resolver->setAllowedTypes('ojs_move_selected_label', ['string']);
+        $resolver->setAllowedTypes('ojs_move_all_label', ['string']);
+        $resolver->setAllowedTypes('ojs_remove_selected_label', ['string']);
+        $resolver->setAllowedTypes('ojs_remove_all_label', ['string']);
+        $resolver->setAllowedTypes('ojs_selected_list_label', ['bool', 'string']);
+        $resolver->setAllowedTypes('ojs_non_selected_list_label', ['bool', 'string']);
+        $resolver->setAllowedTypes('ojs_selector_minimal_height', ['int']);
+        $resolver->setAllowedTypes('ojs_show_filter_inputs', ['bool']);
+        $resolver->setAllowedTypes('ojs_non_selected_filter', ['string']);
+        $resolver->setAllowedTypes('ojs_selected_filter', ['string']);
+        $resolver->setAllowedTypes('ojs_info_text', ['string', 'bool']);
+        $resolver->setAllowedTypes('ojs_info_text_filtered', ['string']);
+        $resolver->setAllowedTypes('ojs_info_text_empty', ['string']);
+        $resolver->setAllowedTypes('ojs_filter_on_values', ['bool']);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options): void
+    {
+        // Options javascript du widget
+        $view->vars['ojs_options'] = $this->getOptionsWidgetCamelized($options);
     }
 
     /**
