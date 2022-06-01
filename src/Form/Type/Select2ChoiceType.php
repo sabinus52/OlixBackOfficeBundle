d0d50d12831140556153315158033a8d90fe7e38
Sabinus52 <sabinus52@gmail.com>
2022-06-01 19:36:35 +0200

    Formalize Select2

--- src/Form/Type/Select2ChoiceType.php
+++ src/Form/Type/Select2ChoiceType.php
@@ -13,56 +13,16 @@ namespace Olix\BackOfficeBundle\Form\Type;
 
 use Olix\BackOfficeBundle\Form\Model\Select2ModelType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormView;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Widget de formulaire de type select amélioré depuis une liste de valeurs.
  *
- * @example     Configuration with options of this type
- * @example     @param Enum   color      : Couleur du widget
- * @example     @param string ajax_route : Name of route for remote data
- * @example     @param int    ajax_count : Max results returned in remote data
- * @example     Config widget with the parameter 'attr' : { params }
- * @example     @param string data-placeholder        : Specifies the placeholder for the control.
- * @example     @param int    data-minimumInputLength : Minimum number of characters required to start a search.
- * @example     @see https://select2.org/configuration/options-api Liste des différentes options
- *
  * @author      Sabinus52 <sabinus52@gmail.com>
  *
- * @see         SwitchModelType::class
+ * @see         Select2ModelType::class
  */
 class Select2ChoiceType extends Select2ModelType
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        parent::configureOptions($resolver);
-
-        $resolver->setDefaults([
-            'ajax_route' => null,
-            'ajax_count' => 25,
-        ]);
-
-        $resolver->setAllowedTypes('ajax_route', ['null', 'string']);
-        $resolver->setAllowedTypes('ajax_count', ['int']);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function buildView(FormView $view, FormInterface $form, array $options): void
-    {
-        parent::buildView($view, $form, $options);
-
-        // pass the form type option directly to the template
-        $view->vars['ajax_route'] = $options['ajax_route'];
-        $view->vars['ajax_count'] = $options['ajax_count'];
-    }
-
     /**
      * {@inheritDoc}
      */
--- src/Form/Type/Select2EntityType.php
+++ src/Form/Type/Select2EntityType.php
@@ -17,16 +17,9 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 /**
  * Widget de formulaire de type select amélioré depuis les données d'une entité.
  *
- * @example     Configuration with options of this type
- * @example     @param Enum   color      : Couleur du widget
- * @example     Config widget with the parameter 'attr' : { params }
- * @example     @param string data-placeholder        : Specifies the placeholder for the control.
- * @example     @param int    data-minimumInputLength : Minimum number of characters required to start a search.
- * @example     @see https://select2.org/configuration/options-api Liste des différentes options
- *
  * @author      Sabinus52 <sabinus52@gmail.com>
  *
- * @see         SwitchModelType::class
+ * @see         Select2ModelType::class
  */
 class Select2EntityType extends Select2ModelType
 {
